cc42d09fe302b06250ae7c9e173aa59f734ac3e3
tesfaye116 <tesfayegirma360@gmail.com>
2022-10-06 23:29:06 +0300

    working with json server

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,70 +1,155 @@
+import axios from "axios";
 import { useState } from "react";
-import "../index.css"
+import "../index.css";
 
 const ModalComponent = () => {
-    const [showModal, setShowModal] = useState(false);
-    return (
-        <>
-            <div className="flex justify-end">
-                <button
-                    className=" bg-blue-900 mt-3  text-white active:bg-dark-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(true)}
-                >
-                    Reg.user
-                </button>
-            </div>
-            {showModal ? (
-                <>
-                    <div
-                        className="modal-user flex-grow-0 justify-end items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none top-0 left-0 right-0 bottom-0"
-                    >
-                        <div className="relative w-auto my-1 mx-auto max-w-2xl">
-                            {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Register user
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    BODY
-                                </div>
-                                {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="bg-blue-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Save Changes
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-75 fixed inset-0 z-40 bg-black"></div>
-                </>
-            ) : null}
-        </>
-    )
-}
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [status, setStatus] = useState(false);
 
-export default ModalComponent
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4000/users", {
+        name,
+        email,
+        gender,
+        status,
+      })
+      .then((res) => {
+        setShowModal(false);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className="flex justify-end">
+        <button
+          className=" bg-blue-500 mt-3  text-white active:bg-dark-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          <i className="fas fa-user-plus"></i>
+        </button>
+      </div>
+      {showModal ? (
+        <>
+          <div className="mt-11 flex-grow-0 justify-end items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none top-0 left-0 right-0 bottom-0 animate-opacity">
+            <div className="relative w-auto my-1 mx-auto max-w-2xl">
+              {/*content*/}
+              <div className="border-0 rounded-md shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none animate-zoom">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t bg-gray-200">
+                  <h3 className="text-2xl text-gray-800 font-bold">
+                    <small>Register</small>
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-1 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-1 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <i className="fas fa-times"></i>
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="name"
+                      >
+                        Name
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name"
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
+                      >
+                        Email
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="gender"
+                      >
+                        Gender
+                      </label>
+                      <select
+                        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-state"
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                      >
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                      </select>
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="status"
+                      >
+                        Status
+                      </label>
+                      <div className="flex">
+                        <input
+                          className="mr-2 leading-tight bg-gray-200"
+                          type="checkbox"
+                          id="status"
+                          value={status}
+                          onChange={(e) => setStatus(e.target.value)}
+                        />
+                        <label className="text-sm" htmlFor="status">
+                          {status ? "Active" : "Inactive"}
+                        </label>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <button
+                        className="w-full bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
+                        Register
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-75 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default ModalComponent;
--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,101 +1,123 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import { Link } from 'react-router-dom'
-import LoadingComponent from "./LoadingComponent"
-import ModalComponent from "./ModalComponent"
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import LoadingComponent from "./LoadingComponent";
+import ModalComponent from "./ModalComponent";
 
 const UserData = () => {
-    const [user, setUsers] = useState([])
-    const [loading, setLoading] = useState(false)
+  const [user, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        setLoading(true)
-        const fetchData = async () => {
-            try {
-                const result = await axios.get("https://gorest.co.in/public/v2/users")
-                setLoading(false)
-                setUsers(result.data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        fetchData()
-    }, [])
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get("http://localhost:4000/users");
+      setLoading(false);
+      setUsers(result.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <>
-            <ModalComponent />
-            {loading ? <LoadingComponent /> :
-                <div className="overflow-x-auto relative mt-1">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto">
-                        <thead className="text-xm  text-gray-700 uppercase bg-blue-200  dark:bg-gray-700 dark:text-white">
-                            <tr>
-                                <th scope="col" className="py-3 px-6">
-                                    SN
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    Name
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    Email
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    Gender
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    Status
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    post
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                user.map(item => (
-                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={item.id}>
-                                        <td className=" font-medium text-gray-900 whitespace-nowrap dark:text-white py-4 px-6">
-                                            {item.id}
-                                        </td>
-                                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {item.name}
-                                        </th>
-                                        <td className="py-4 px-6 text-sm leading-5 text-gray-500 dark:text-gray-400">
-                                            {item.email}
-                                        </td>
-                                        <td className="py-4 px-6">
-                                            {item.gender}
-                                        </td>
-                                        <td className="py-4 px-6">
-                                            {item.status === "active" ? <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-green-100 text-green-800">
-                                                Active
-                                            </span> : <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-red-100 text-red-800">
-                                                Inactive
-                                            </span>}
-                                        </td>
-                                        <td className="py-4 px-6">
-                                            <Link to={`/${item.id}/posts`}>
-                                                <pre className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-green-100 text-green-800 cursor-pointer">
-                                                    <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
-                                                    See more
-                                                </pre>
+  return (
+    <>
+      <ModalComponent />
+      {loading ? (
+        <LoadingComponent />
+      ) : (
+        <div className="overflow-x-auto relative mt-1">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto">
+            <thead className="text-xm  text-gray-700 uppercase bg-blue-200  dark:bg-gray-700 dark:text-white">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  SN
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Name
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Email
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Gender
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Status
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  post
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {user.map((item) => (
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  key={item.id}
+                >
+                  <td className=" font-medium text-gray-900 whitespace-nowrap dark:text-white py-4 px-6">
+                    {item.id}
+                  </td>
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {item.name}
+                  </th>
+                  <td className="py-4 px-6 text-sm leading-5 text-gray-500 dark:text-gray-400">
+                    {item.email}
+                  </td>
+                  <td className="py-4 px-6">{item.gender}</td>
+                  <td className="py-4 px-6">
+                    {item.status ? (
+                      <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    ) : (
+                      <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-red-100 text-red-800">
+                        Inactive
+                      </span>
+                    )}
+                  </td>
+                  <td className="py-4 px-6">
+                    <Link>
+                      <pre className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-green-100 text-green-800 cursor-pointer">
+                        <svg
+                          className="w-5 h-5 mr-1"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                          />
+                        </svg>
+                        See more
+                      </pre>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
+};
 
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-
-                        </tbody>
-                    </table>
-                </div>
-
-            }
-        </>
-    )
-}
-
-export default UserData
+export default UserData;
