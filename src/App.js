dcd34492ec7fdee3cb97b1e2fa9ef11de4ef4c04
Tesfaye Girma <tesfayegirma360@gmail.com>
2022-10-06 16:12:16 +0300

    modal and router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import UserData from "./components/UserData";
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <>
+    <Router>
       <div className="w-100 flex justify-center mx-auto px-4">
         <div className="container">
           <NavBar />
-          <UserData />
+          <Routes>
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/users" exact element={<UserData />} />
+          </Routes>
         </div>
       </div>
-    </>
+    </Router>
   );
 }
 
